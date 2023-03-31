7e28e23a4ec5b124e495bfaafe954958b1ed8afe
Ibrahim Olasunkanmi <ibrahimolasunkanmi98@gmail.com>
2023-03-31 18:33:44 +0100

    Update handlers.c

--- handlers.c
+++ handlers.c
@@ -13,7 +13,7 @@
  * Return: Number of chars printed.
  */
 int handle_write_char(char c, char buffer[],
-	int flags, int width, int precision, int size)
+	int flags, int width, int precision, int size) /* Prototype */
 { /* char is stored at left and paddind at buffer's right */
 	int i = 0;
 	char padd = ' ';
